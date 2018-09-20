605740ed20b68798aa781762f77e29b5c6206c02
olindos <yason4ik87@gmail.com>
2018-09-20 13:05:15 +0300

    Change action accounts_controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,16 @@
 class AccountsController < ApplicationController
   def new
+    @account = Account.new
   end
 
   def create
+    @account = Account.new(account_params)
+    @account.save
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:balance, :currency, :name, :email)
   end
 end

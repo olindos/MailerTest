6a8433e0dabb2c6369f8a5e591f2c64426d49ebe
olindos <yason4ik87@gmail.com>
2018-09-20 13:19:46 +0300

    Add action send_balance_mail with messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,13 @@ class MessagesController < ApplicationController
     @message.save
   end
 
+  def send_balance_mail
+    @account = Account.find(params[:id])
+    @message = Message.find(params[:id])
+
+    AccountMailer.account_balance(@message, @account).deliver_now
+  end
+
   private
 
   def message_params

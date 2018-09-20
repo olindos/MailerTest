72501c68c0af1f33100d0a0688ebd9397d0b8ac1
olindos <yason4ik87@gmail.com>
2018-09-20 13:25:42 +0300

    Add method in AccountMailer

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,2 +1,8 @@
 class AccountMailer < ApplicationMailer
+
+  def account_balance
+    email_with_name = %("#{@account.name}" <#{@account.email}>)
+    mail(to: email_with_name, subject: @message.title)
+  end
+
 end
